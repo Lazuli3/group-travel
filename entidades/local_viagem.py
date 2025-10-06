a632d68e16767dbb3da99d93d63e8ccee9696f93
sarahmeivie <talimeireles@gmail.com>
2025-10-06 14:23:12 -0300

    criação de tela_local_viagem + checando atributos na classe LocalViagem

--- entidades/local_viagem.py
+++ entidades/local_viagem.py
@@ -1,12 +1,21 @@
-from datetime import datetime
 
 class LocalViagem:
-    
+
     def __init__(self, cidade: str, pais: str):
         if not isinstance(cidade, str):
-            raise TypeError ("cidade deve ser uma instância da classe str.")
+            raise TypeError ("Cidade deve ser uma instância da classe str.")
         if not isinstance(pais, str):
-            raise TypeError ("pais deve ser uma instância da classe str.")
+            raise TypeError ("País deve ser uma instância da classe str.")
+
+        if not cidade.strip():
+            raise ValueError("Cidade não pode ser vazia.")
+        if any(c.isdigit() for c in cidade):
+            raise ValueError("Cidade não pode conter números.")
+
+        if not pais.strip():
+            raise ValueError("País não pode ser vazio.")
+        if any(c.isdigit() for c in pais):
+            raise ValueError("País não pode conter números.")
 
         self.__cidade = cidade
         self.__pais = pais
@@ -14,18 +23,15 @@ class LocalViagem:
     @property
     def cidade(self):
         return self.__cidade
-    
+
     @cidade.setter
     def cidade(self, cidade):
         self.__cidade = cidade
-    
+
     @property
     def pais(self):
         return self.__pais
-    
+
     @pais.setter
     def pais(self, pais):
         self.__pais = pais
-        
-
-        
---
+++ view/tela_local_viagem.py
@@ -0,0 +1,38 @@
+
+class TelaLocalViagem:
+
+    def mostra_mensagem(self, msg:str):
+        print(msg)
+    
+    def mostra_opcoes(self):
+        '''
+            ============ Menu ============')
+            1 - Incluir local de viagem")
+            2 - Listar local de viagem')
+            3 - Excluir local de viagem')
+            0- Sair
+        '''
+
+        try:
+            return int(input('Escolha uma das opções do menu: '))
+        
+        except ValueError:
+            self.mostra_mensagem("Escolha uma opção válida do menu.")
+            return self.mostra_opcoes()
+
+    def pega_dados_local_viagem(self):
+        print('============ Cadastro ============')
+        cidade = input('Nome da cidade: ')
+        pais = input('Nome do país: ')
+        return {
+            'cidade': cidade,
+            'pais': pais
+        }
+    
+    def lista_locais_viagem(self, locais_viagem:list):
+        if not locais_viagem:
+            self.mostra_mensagem("Nenhum local foi cadastrado.")
+        
+        print('============ Lista de locais ============')
+        for {i}, local in enumerate(locais_viagem, 1):
+            print(f"Cidade: {local.cidade} | País: {local.pais}")
