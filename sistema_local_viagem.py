064a43c4b4ccf03bb62442e374fc9b7f7c3f4bda
Pietra_M0ta <laismotadev@gmail.com>
2025-10-12 09:31:14 -0300

    estruturando o mvc de todos os objetos

--- sistema_local_viagem.py
+++
@@ -1,5 +0,0 @@
-from controladores.controlador_local_viagem import ControladorLocalViagem
-
-if __name__ == "__main__":
-    controlador = ControladorLocalViagem()
-    controlador.inicia()
--- sistema_pessoa.py
+++
@@ -1,5 +0,0 @@
-# arquivo: sistema_pessoa.py
-from controladores.controlador_pessoa import ControladorPessoa
-
-if __name__ == "__main__":
-    ControladorPessoa().inicia()
