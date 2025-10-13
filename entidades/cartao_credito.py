168910c59b86ba1f675a3bf8eef9bd5c634d9988
sarahmeivie <talimeireles@gmail.com>
2025-10-13 11:58:40 -0300

    mudança de outros nomes

--- entidades/cartao_credito.py
+++ entidades/cartao_credito.py
@@ -2,26 +2,26 @@ from pagamento import Pagamento
 
 class CartaoCredito(Pagamento):
 
-    def __init__(self, pagante, valor, n_cartao:int, bandeira:str, n_parcelas:int):
+    def __init__(self, pagante, valor, num_cartao:int, bandeira:str, parcelas:int):
         super().__init__(pagante, valor)
-        self.__n_cartao = 0
+        self.__num_cartao = 0
         self.__bandeira = None
-        self.__n_parcelas = 0
+        self.__parcelas = 0
 
-        if isinstance(n_cartao, int):
-            self.__n_cartao = n_cartao
+        if isinstance(num_cartao, int):
+            self.__num_cartao = num_cartao
         if isinstance(bandeira, str):
             self.__bandeira = bandeira
-        if isinstance(n_parcelas, int):
-            self.__n_parcelas = n_parcelas
+        if isinstance(parcelas, int):
+            self.__parcelas = parcelas
 
     
     @property
-    def n_cartao(self):
-        return self.__n_cartao
-    @n_cartao.setter
-    def n_cartao(self, n_cartao):
-        self.__n_cartao = n_cartao
+    def num_cartao(self):
+        return self.__num_cartao
+    @num_cartao.setter
+    def n_cartao(self, num_cartao):
+        self.__num_cartao = num_cartao
     
     @property
     def bandeira(self):
@@ -31,8 +31,8 @@ class CartaoCredito(Pagamento):
         self.__bandeira = bandeira
     
     @property
-    def n_parcelas(self, n_parcelas):
-        return self.__n_parcelas
-    @n_parcelas.setter
-    def n_parcelas(self, n_parcelas):
-        self.__n_parcelas = n_parcelas
+    def parcelas(self, parcelas):
+        return self.__parcelas
+    @parcelas.setter
+    def parcelas(self, parcelas):
+        self.__parcelas = parcelas
