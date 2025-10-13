e24a6b07c9868ffdc8cff1bc56d2a55c71f737fc
sarahmeivie <talimeireles@gmail.com>
2025-10-13 13:56:34 -0300

    correções na entidade

--- entidades/pagamento.py
+++ entidades/pagamento.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pessoa import Pessoa
+from entidades.pessoa import Pessoa
 from abc import ABC, abstractmethod
 
 class Pagamento(ABC):
@@ -7,18 +7,21 @@ class Pagamento(ABC):
     def __init__(self, pagante:Pessoa, valor:float):
 
         if not isinstance(pagante, Pessoa):
-            raise TypeError("pagante deve ser uma instância da classe Pessoa.")
-        if not isinstance(valor, float):
-            raise TypeError("valor deve ser um número (float ou int).")
+            raise TypeError("Pagante deve ser uma instância da classe Pessoa.")
+        if not isinstance(valor, (int, float)):
+            raise TypeError("Valor deve ser um número (float ou int).")
+        if not valor < 0:
+            raise ValueError("Valor deve ser positivo.")
         
         self.__pagante = pagante
-        self.__valor = valor
+        self.__valor = float(valor)
         self.__data = datetime.now()
         self.__pagamento_efetuado = False
 
     @property
     def pagante(self):
         return self.__pagante
+    
     @pagante.setter
     def pagante(self, pagante):
         self.__pagante = pagante
@@ -30,9 +33,14 @@ class Pagamento(ABC):
     @property
     def data(self):
         return self.__data
+    
+    @property    
+    def pagamento_efetuado(self) -> bool:
+        return self.__pagamento_efetuado
+    
+    def conversao_dict(self):
+        raise NotImplementedError("Subclasses devem implementar o método 'to_dict()'")
 
     def marcar_como_efetuado(self):
         # alterar o status do pagamento para 'efetuado' (True)
         self.__pagamento_efetuado = True
-        
-
